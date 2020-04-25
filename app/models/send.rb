fb9b67bfdd8a74cd188310b4f1a6d2c6811b38ca
fadhili-ed <fadhilied00@gmail.com>
2020-04-25 11:45:27 +0300

    ensure pin, amount and id_number are numbers only

--- app/models/send.rb
+++ app/models/send.rb
@@ -1,7 +1,8 @@
 class Send < ApplicationRecord
     validates :sender_phone_number, presence: true
     validates :recepient_phone_number, presence: true
-    validates :pin, presence: true
-    validates :amount, presence: true
-    validates :sender_id_number, presence: true
+    validates :pin, presence: true, numericality: {only_integer: true}
+    validates :amount, presence: true, numericality: {only_integer: true}
+    validates :sender_id_number, presence: true, length: {maximum: 9, minimum: 5},
+                numericality: { only_integer: true}
 end
--- spec/models/send_spec.rb
+++ spec/models/send_spec.rb
@@ -16,5 +16,9 @@ RSpec.describe Send, type: :model do
     it { is_expected.to validate_presence_of(:sender_phone_number)}
     it { is_expected.to validate_presence_of(:amount)}
     it { is_expected.to validate_presence_of(:pin)}
+    it { is_expected.to validate_length_of(:sender_id_number)}
+    it { is_expected.to validate_numericality_of(:amount)}
+    it { is_expected.to validate_numericality_of(:pin)}
+    it { is_expected.to validate_numericality_of(:sender_id_number)}
   end
 end
