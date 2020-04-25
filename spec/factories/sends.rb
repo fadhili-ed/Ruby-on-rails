073d91c725d8277b4edbf8b13c4627b2e4ac0786
fadhili-ed <fadhilied00@gmail.com>
2020-04-25 12:47:29 +0300

    using FactoryBot to test attributes

--- spec/factories/sends.rb
+++ spec/factories/sends.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :send do
-    sender_phone_number { "MyString" }
-    recepient_phone_number { "MyString" }
-    sender_id_number { "MyString" }
-    amount { "MyString" }
-    pin { "MyString" }
+    sender_phone_number { "0702564851" }
+    recepient_phone_number { "0745896239" }
+    sender_id_number { "234587" }
+    amount { "500" }
+    pin { "1234" }
   end
 end
--- spec/models/send_spec.rb
+++ spec/models/send_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Send, type: :model do
+  it 'should have valid Factory' do
+    expect(FactoryBot.create(:user)).to be_valid
+  end
+
   describe "#sends table column" do
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :sender_phone_number }
@@ -22,6 +26,9 @@ RSpec.describe Send, type: :model do
     it { is_expected.to validate_numericality_of(:amount)}
     it { is_expected.to validate_numericality_of(:pin)}
     it { is_expected.to validate_numericality_of(:sender_id_number)}
+    it "ensures sender phone number and recepirnt phone numbers are not equal" do
+      expect(:sender_phone_number).to_not eq :recepient_phone_number
+    end
   end
 
   it "ensures db is safe  and attributes are not null" do
@@ -40,5 +47,22 @@ RSpec.describe Send, type: :model do
     expect { send_without_recepient_phone_number.save!(validate: false)}.to raise_error(ActiveRecord::NotNullViolation)
   end
 
-  
+  describe "#sending money" do
+    it "ensures sending has valid attributes" do
+      sends = FactoryBot.create(:send)
+      expect(sends.pin).to eq "1234"
+      expect(sends.sender_phone_number).to eq "0702564851"
+      expect(sends.recepient_phone_number).to eq "0745896239"
+      expect(sends.sender_id_number).to eq "234587"
+      expect(sends.amount).to eq "500"
+    end
+
+    it "only allows id_number that are digits" do
+      sends = FactoryBot.build(:send, sender_id_number: "abc56890")
+      sends.save
+      expect(sends).to_not be_valid
+      expect(sends.errors.messages[:sender_id_number]).to eq ["is not a number"]
+    end
+  end
+
 end
