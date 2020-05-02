4fbb9ebac40508dbf70bb3d11b583d8848f7be7d
fadhili-ed <fadhilied00@gmail.com>
2020-05-02 11:50:26 +0300

    delete files for articles blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   get 'statics/home'
-  # get 'welcome/index'
-  
-  # resources :articles do
-  #   resources :comments
-  # end
   root to: 'statics#home'
   
-  # root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'users/new', to: 'users#new', as: 'new_user'
   post 'users/', to: 'users#create'
@@ -24,7 +18,6 @@ Rails.application.routes.draw do
 
   get '/withdrawals/new', to: 'withdrawals#new', as: 'new_withdrawal'
   post 'withdrawals/', to: 'withdrawals#create'
-
 
   get 'send/new', to: 'sends#new', as: 'new_send'
   post 'send/new', to: 'sends#create'
