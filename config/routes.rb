8d485c56665cb0262912c11200b83a4e9b03797e
fadhili-ed <fadhilied00@gmail.com>
2020-05-03 12:52:43 +0300

    delete files dealing with articles blog

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 
 Rails.application.routes.draw do
   get 'statics/home'
-  get 'welcome/index'
 
   root to: 'statics#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
