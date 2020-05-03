f76e6e958aa38f228ee3f5a811be43ed0d4ede69
fadhili-ed <fadhilied00@gmail.com>
2020-05-03 12:56:57 +0300

    delete files having to deal with articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
   get 'statics/home'
-  get 'welcome/index'
-  
-  resources :articles do
-    resources :comments
-  end
-
-  # root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'users/new', to: 'users#new', as: 'new_user'
   post 'users/', to: 'users#create'
